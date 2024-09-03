4ce1b072c60426b1b834cb06ecffd235932f80c9
signomarco <signomarco2003@gmail.com>
2024-09-04 00:00:50 +0200

    Section 03_ticket Finished

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,88 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+fn validate_product_name(name: &String) -> bool {
+    if name.is_empty() || name.len() > 300 {
+        false
+    } else { true }
+}
+
+fn validate_quantity(quantity: &u16) -> bool {
+    if quantity <= &0 {
+        false
+    } else { true }
+}
+
+fn validate_unit_price(price: &u16) -> bool {
+    if price <= &0 {
+        false
+    } else { true }
+}
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16
+}
+
+impl Order {
+    // Constructor
+    pub fn new(name: String, quantity: u16, unit_price: u16) -> Self {
+        if !validate_product_name(&name) {
+            panic!();
+        }
+        if !validate_quantity(&quantity) {
+            panic!()
+        }
+        if !validate_unit_price(&unit_price) {
+            panic!()
+        }
+
+        Self {
+            product_name: name,
+            quantity,
+            unit_price
+        }
+    }
+
+    // setters
+    pub fn set_product_name(&mut self, name: String) {
+        if !validate_product_name(&name) {
+            panic!();
+        }
+        self.product_name = name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u16) {
+        if !validate_quantity(&quantity) {
+            panic!()
+        }
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u16) {
+        if !validate_unit_price(&unit_price) {
+            panic!()
+        }
+        self.unit_price = unit_price;
+    }
+
+    // getters
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+
+    // Utilities
+    pub fn total(&self) -> u16 {
+        self.quantity * self.unit_price
+    }
+}
